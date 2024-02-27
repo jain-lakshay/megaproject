ccd5de6081b3554effffb7dfc41f090a34b27b55
lakshay jain <153222886+jain-lakshay@users.noreply.github.com>
2024-02-27 18:18:06 +0000

    added user model part 1

--- models/user.schema.js
+++ models/user.schema.js
@@ -1,5 +1,7 @@
 import mongoose from "mongoose";
 
+import AuthRoles from "../utils/authRoles";
+
 const userSchema = mongoose.Schema(
     {
 
@@ -7,6 +9,31 @@ const userSchema = mongoose.Schema(
             type: String,
             required:[true," the name is required"],
             maxLength:[50,"maximum length is 50"]
-        }
+        },
+        email:{
+            type: String,
+            required:[true," the eamil is required"],
+           unique: true
+        },
+       password:{
+            type: String,
+            required:[true," the password is required"],
+           minLength:[5,"minimum  length is 5"],
+           select:false,
+        },
+        role:{
+            type:String,
+            enum:Object.values(AuthRoles),
+            default:AuthRoles.USER
+        },
+        forgotPasswordToken:String,
+        forgotPasswordExpiry:Date,
+    },
+    {
+        timestamps:true
     }
-)
+);
+
+export default mongoose.model("User",userSchema)
+
+
