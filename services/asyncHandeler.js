f5eb6a8f06b26d5f61bdb473758984efbf168f4b
lakshay jain <153222886+jain-lakshay@users.noreply.github.com>
2024-02-29 07:57:14 +0000

    design method for asyncHandler and error

---
+++ services/asyncHandeler.js
@@ -0,0 +1,17 @@
+const asyncHandler = (fn)=> async (req,res,next)=>{
+    try {
+        
+       await fn (req,res,next)
+    } catch (err) {
+        res.status(500).json({
+         success:false,
+         message:err.message   
+        })
+    }
+
+
+}
+
+
+
+export default asyncHandler;
