f703f92608a26cc704c26070460fb4c1cc318044
lakshay jain <153222886+jain-lakshay@users.noreply.github.com>
2024-02-29 18:22:04 +0000

    created the mail utility

---
+++ utils/mailHelper.js
@@ -0,0 +1,18 @@
+import  transporter from '../config/transporter.config'
+import config from '../config'
+
+
+
+const mailHelper = async(options)=>{
+const message ={
+    from: config.SMTP_MAIL_EMAIL, // sender address
+    to: options.email, // list of receivers
+    subject: options.subject, // Subject line
+    text: options.text, // plain text body
+   
+}
+await transporter.sendMail(message)
+}
+
+
+export default mailHelper
