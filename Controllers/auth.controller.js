01f71817d8d689efc0f56f7ffc56ef1b759a4be2
lakshay jain <153222886+jain-lakshay@users.noreply.github.com>
2024-03-01 19:20:04 +0000

    auth controller - reset password added

--- Controllers/auth.controller.js
+++ Controllers/auth.controller.js
@@ -1,7 +1,8 @@
  import User from '../models/user.schema'
  import asyncHandler from '../services/asyncHandler'
  import CustomError from '../utils/customError'
-
+import mailHelper from '../utils/mailHelper'
+import crypto from 'crypto'
 
  export const cookieOptions = {
     expires: new Date(Date.now()+3*24*60*60*100),
@@ -63,7 +64,7 @@ if(!email||!password){
    throw  new CustomError('please fill the required field',400)
 }
 //find user
-const user = User.findOne({email}).select("+password")//read documentation moongoose select
+const user = await User.findOne({email}).select("+password")//read documentation moongoose select
 if(!user){
    throw new CustomError('Invalid Credantial',400)
 }
@@ -101,3 +102,95 @@ throw new CustomError('Invalid Credential')
       message:"loggedout"
    })
  })
+
+ /******************************************************
+ * @FORGOT_PASSWORD
+ * @route http://localhost:5000/api/auth/password/forgot
+ * @description User will submit email and we will generate a token
+ * @parameters  email
+ * @returns success message - email send
+ ******************************************************/
+
+ export const forgotPassword= asyncHandler (async (req,res)=>{
+ const {email}=req.body
+ const user =  await User.findOne({email})
+
+ if (!user){
+   throw new CustomError('error user not found',404)
+ }
+//reset token
+  const resetToken=user.generateForgotPasswordToken()
+   await user.save({validateBeforeSave: false})
+
+    const resetUrl=
+    `${req.protocol}://${req.get("host")}/api/auth/password/reset/${resetToken}`
+
+const text =`your password reset url is  /n/n ${resetUrl}/n/n`
+
+    try {
+      
+await mailHelper({
+
+   email:user.email,
+   subject:"password reset email for website",
+   text:text,
+})
+res.status(200).json({
+   success:true,
+   message:`email send to ${user.email}`
+})
+    } catch (err) {
+//role back clear fields and save
+user.forgotPasswordToken=undefined
+user.forgotPasswordExpiry=undefined
+
+await user.save({validateBeforeSave:false})
+
+      throw new CustomError(err.message||'email sent failure',500)
+    }
+ })
+
+ /******************************************************
+ * @RESET_PASSWORD
+ * @route http://localhost:5000/api/auth/password/reset/:resetToken
+ * @description User will be able to reset password based on url token
+ * @parameters  token from url, password and confirmpass
+ * @returns User object
+ ******************************************************/
+export const resetPassword = asyncHandler(async(req,res)=>{
+   const {token:resetToken}=req.params
+   const {password,confirmPassword}=req.body
+
+   const resetPasswordToken=crypto
+   .createHash('sha256')
+   .update(resetToken)
+   .digest(hex)
+
+  const user=  await User.findOne({
+      forgotPasswordToken:resetPasswordToken,
+      forgotPasswordExpiry:{$gt: Date.now()}
+   });
+   if(!user){
+      throw  new CustomError('password token is expired or invalid',400)
+   }
+   if(password!== confirmPassword){
+      throw new CustomError('password and confirm password should be same',400)
+   }
+
+   user.password=password
+   user.forgotPasswordToken=undefined
+   user.forgotPasswordExpiry=undefined
+
+   await user.save()
+})
+
+
+    //create token and send as response
+    const token = user.getJwtToken()
+    user.password=undefined;
+
+    res.cookie('token',token,cookieOptions)
+
+res.status(200).json({
+   success:true
+})
